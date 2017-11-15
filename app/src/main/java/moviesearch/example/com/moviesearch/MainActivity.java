99255cd9353e4e5ba05249b0665019ddead36bac
Nivedita-puneet <nivedita.puneet@gmail.com>
2017-11-15 12:21:54 +1100

    Updated TMDB URL.

--- app/src/main/java/moviesearch/example/com/moviesearch/MainActivity.java
+++ app/src/main/java/moviesearch/example/com/moviesearch/MainActivity.java
@@ -114,6 +114,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             @Override
             public boolean onQueryTextChange(String newText) {
 
+                showErrorMessage();
                 MainActivity.this.query = newText;
                 return true;
             }
