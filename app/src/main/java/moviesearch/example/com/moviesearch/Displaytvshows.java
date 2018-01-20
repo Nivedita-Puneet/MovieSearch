9181635be97edeb1b3207f17f4ecc68fec641dcb
Nivedita-puneet <nivedita.puneet@gmail.com>
2018-01-20 13:06:46 +1100

    Rxandroid to display tv shows

--- app/src/main/java/moviesearch/example/com/moviesearch/Displaytvshows.java
+++ app/src/main/java/moviesearch/example/com/moviesearch/Displaytvshows.java
@@ -1,10 +1,121 @@
 package moviesearch.example.com.moviesearch;
 
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import moviesearch.example.com.moviesearch.data.Tvshow;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by PUNEETU on 18-01-2018.
  */
 
-public class Displaytvshows extends AppCompatActivity{
+public class Displaytvshows extends AppCompatActivity {
+
+    SearchView searchTvShows;
+    String searchQuery;
+    private RecyclerView mtvshowsview;
+    ProgressBar progressBar;
+    private Subscription showSubscription;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        this.getSupportActionBar().setDisplayShowTitleEnabled(false);
+        setContentView(R.layout.display_tv_shows);
+        initializeControls();
+        createObservable();
+    }
+
+    private void initializeControls() {
+
+        mtvshowsview = (RecyclerView) findViewById(R.id.recyclerview_tv_shows);
+        progressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator_tv_shows);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        getSearchViewFromMenu(menu);
+        return true;
+    }
+
+    private void getSearchViewFromMenu(Menu menu) {
+        MenuItem searchItem = menu.findItem(R.id.search);
+        searchTvShows = (SearchView) MenuItemCompat.getActionView(searchItem);
+        if (!TextUtils.isEmpty(Displaytvshows.this.searchQuery)) {
+            searchTvShows.setQuery(Displaytvshows.this.searchQuery, false);
+            searchTvShows.clearFocus();
+        }
+        handleSearchQuery(searchTvShows);
+    }
+
+    private void createObservable() {
+        Observable<List<Tvshow>> tvShowObservable
+                = Observable.fromCallable(new Callable<List<Tvshow>>() {
+            @Override
+            public List<Tvshow> call() throws Exception {
+                return null;
+            }
+        });
+        showSubscription = tvShowObservable.
+                subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<List<Tvshow>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(List<Tvshow> tvshows) {
+                        /*Populate recycler view and bind it to an adapter*/
+                    }
+                });
+    }
+
+    private void handleSearchQuery(SearchView searchView) {
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
---
+++ app/src/main/java/moviesearch/example/com/moviesearch/data/Tvshow.java
@@ -0,0 +1,47 @@
+package moviesearch.example.com.moviesearch.data;
+
+/**
+ * Created by PUNEETU on 20-01-2018.
+ */
+
+public class Tvshow {
+
+    private String poster;
+    private String title;
+    private String genre;
+    private String plot;
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+
+}
